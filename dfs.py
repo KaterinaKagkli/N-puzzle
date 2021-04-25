73dac2b6abe3d9aba7a23092bc9ccc90573a6b08
Katerina Kagkli <katerinakgl@outlook.com.gr>
2021-04-25 23:11:37 +0300

    Made the algorithm iterative rather than recursive

--- dfs.py
+++ dfs.py
@@ -1,67 +1,58 @@
 import random
 import copy
 import sys
-import math
 
 
 def main():
+    # Setting initial state
     initial_state = creating_initial_state()
-    goal = [[1, 2, 3], [4, 5, 6], [7, 8, None]]         # Setting puzzle goal
+    # initial_state = [[1, 2, 3], [4, 5, 6], [7, None, 8]]
+    # Setting puzzle goal
+    goal = [[1, 2, 3], [4, 5, 6], [7, 8, None]]
+    # Initializing frontier and setting initial state as frontier
     frontier = []
     frontier.insert(0, initial_state)
+    # Initializing close set
     close_set = []
-
-    # Starting solution with the initial state as first iteration
-    begin_solution(frontier, goal, close_set)
-
-
-def begin_solution(frontier, goal, close_set):
-    set_current_state(frontier, goal, close_set)
+    # Starting solution with initial state as first iteration
+    solution(frontier, goal, close_set)
 
 
-def set_current_state(frontier, goal, close_set):
-    new_current_state = frontier[0]
-    print_array(new_current_state)
-    # If current state exists in closed set skip to the next in the frontier
-    if new_current_state in close_set:
-        children = []
-        set_frontier(frontier, children, goal, close_set)
-
-    if new_current_state == goal:
-        close_set = set_closed_set(frontier[0], close_set)
-        print("Solution found")
-        for i in close_set:
-            print_array(i)
+def solution(frontier, goal, close_set):
+    while True:
+        print("Frontier length")
+        print(len(frontier))
+        current_state = set_current_state(frontier)
+        if current_state == goal:
+            print("Solution found! \nSteps: ")
+            for i in close_set:
+                print_array(i)
+            print_array(goal)
+            print("Solution found! See steps above.")
+            print("Number of steps for solution: ")
+            print(len(close_set))
             sys.exit()
-    find_children(frontier, new_current_state, goal, close_set)
+        if current_state in close_set:
+            frontier.pop(0)
+        else:
+            children = find_children(current_state)
+            close_set = set_close_set(current_state, close_set)
+            frontier = set_frontier(frontier, children)
+        if not frontier:
+            print("Searching frontier is empty. \nCould not find solution. \nExiting program...")
 
 
-def set_frontier(frontier, children, goal, close_set):
-    # Depth first search: prioritizes children
-    close_set = set_closed_set(frontier[0], close_set)
-    if not children:
-        frontier.pop(0)
-        set_current_state(frontier, goal, close_set)
-    else:
-        for i in children:
-            frontier.insert(0, i)
-        set_current_state(frontier, goal, close_set)
+# Setting new current state that is to be examined
+def set_current_state(frontier):
+    new_current_state = frontier[0]
+    return new_current_state
 
 
-def set_closed_set(state, close_set):
-    if state not in close_set:
-        close_set.append(state)
-    if len(close_set) == math.factorial(9):
-        print("Max possible combination")
-    return close_set
-
-
-# Finding children of current (2)
-def find_children(frontier, current_state, goal, close_set):
+# Finding children of current state
+def find_children(current_state):
     children = []
-
     # Calling neighbors function to locate the neighbors of null, thus locating the children
-    neighbors = find_neighbors(current_state, close_set)
+    neighbors = find_neighbors(current_state)
 
     for z in range(len(neighbors)):
         # Resetting child with current state information after each iteration.
@@ -79,17 +70,16 @@ def find_children(frontier, current_state, goal, close_set):
             for y in range(3):
                 if current_state[x][y] == neighbors[z]:
                     child[x][y], child[i][j] = child[i][j], child[x][y]
-
                     break
             else:
                 continue
             break
         children.append(child)
+    return children
 
-    set_frontier(frontier, children, goal, close_set)
 
-# Finding neighbors of null cell (3)
-def find_neighbors(current_state, close_set):
+# Finding neighbors of null cell of current state
+def find_neighbors(current_state):
     neighbors = []
     for i in range(3):
         for j in range(3):
@@ -116,7 +106,7 @@ def find_neighbors(current_state, close_set):
                         neighbors.append(current_state[i][j + 1])
                         neighbors.append(current_state[i][j - 1])
                     if i == 2:
-                        neighbors.append(current_state[i][j+1])
+                        neighbors.append(current_state[i][j + 1])
                         neighbors.append(current_state[i - 1][j])
                         neighbors.append(current_state[i][j - 1])
                 if j == 2:
@@ -133,22 +123,44 @@ def find_neighbors(current_state, close_set):
     return neighbors
 
 
-# Creating initial state (1)
-def creating_initial_state():
-    possible_numbers = [1, 2, 3, 4, 5, 6, 7, 8]         # Available numbers that can be used in puzzle
-    init_state = [[0, 0, 0], [0, None, 0], [0, 0, 0]]   # Initializing the list
+# Setting close set
+def set_close_set(state, close_set):
+    close_set.append(state)
+    return close_set
 
+
+# Setting frontier
+def set_frontier(frontier, children):
+    # Popping first element of frontier (the one that was just examined)
+    frontier.pop(0)
+    # Inserting children in frontier. Dfs algorithm prioritizes children, thus, children will be inserted in the front
+    for i in children:
+        frontier.insert(0, i)
+    return frontier
+
+
+# Creating initial state
+def creating_initial_state():
+    # Available numbers that can be used in puzzle
+    possible_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    # Initializing the list
+    init_state = [[0, 0, 0], [0, None, 0], [0, 0, 0]]
     for i in range(3):
         for j in range(3):
             if init_state[i][j] is not None:
-                x = random.choice(possible_numbers)     # Choosing random number from possible numbers
-                possible_numbers.remove(x)              # Removing the number that was chosen from possible numbers
-                init_state[i][j] = x                    # Assigning number to puzzle's cell
+                # Choosing random number from possible numbers
+                x = random.choice(possible_numbers)
+                # Removing the number that was chosen from possible numbers
+                possible_numbers.remove(x)
+                # Assigning number to puzzle's cell
+                init_state[i][j] = x
+    print("Initial state: ")
     print_array(init_state)
     return init_state
 
 
-def print_array(e):                              # Print list as array (N)
+# Print list as array
+def print_array(e):
     for i in range(3):
         for j in range(3):
             if e[i][j] is None:
@@ -160,5 +172,4 @@ def print_array(e):                              # Print list as array (N)
 
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(10**9)
     main()
